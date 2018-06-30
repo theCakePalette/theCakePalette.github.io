196e65efc811cf8c21eca648ec9b524ff56bed24
theCakePalette <thecakepalettebyzainab@gmail.com>
2018-06-30 19:10:35 +0530

    commit 3.1
    Contact Us Form changed to Mailto

--- js/contact.js
+++ js/contact.js
@@ -20,19 +20,4 @@ $(document).ready(function(){
        		$(".btn-open").css('display','block') ;
        }
   });
-
-  $("input[type=button]").click(function(e){
-    e.preventDefault();
-    $.ajax({
-      type: 'post',
-      url: '../php/contact.php',
-      data: $('.content-about form').serialize(),
-      success: function(data){
-        $("#contact-lonavala").append("<br><div align='center' class='thank'>Thank You! We will get back to you via email.<div>");
-      }
-    });
-
-  });
-
-
 });
