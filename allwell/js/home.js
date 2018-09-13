2716f93b15dbe67b49535effc92f7d6f9fdd78b0
theCakePalette <thecakepalettebyzainab@gmail.com>
2018-09-14 02:57:31 +0530

    allwell check
    this is just a temporary commit

---
+++ allwell/js/home.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$(".button a").click(function(){
+        $(".overlay").fadeToggle(200);
+	    if ( $(".btn-open").css('display') != 'none' ){
+		    $(".btn-open").css('display','none');
+		    $(".wrap-menu").css('display','block');
+		    $(".btn-close").css('display','block') ;
+	    }else{
+       		$(".btn-close").css('display','none');
+       		$(".btn-open").css('display','block') ;
+		    $(".wrap-menu").css('display','none');
+	    }
+    });
+});
